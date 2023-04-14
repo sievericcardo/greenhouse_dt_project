929abcb334d92486229dcab6790f8da38833745a
marcooamato <amatomarco111@gmail.com>
2023-04-14 12:16:01 +0200

    SMOL runner with non static path. getAssetModel() getSettings()
    Co-authored-by: Gianluca Bàrmina <N-essuno@users.noreply.github.com>

--- smol_runner/src/main/java/SmolRunner.java
+++ smol_runner/src/main/java/SmolRunner.java
@@ -1,32 +1,62 @@
 import no.uio.microobject.main.Settings;
 import no.uio.microobject.runtime.REPL;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.HashMap;
 
 public class SmolRunner {
-    public static void main(String[] args) {
-        // get current time in milliseconds
-        long timeMilli = System.currentTimeMillis();
+    static boolean verbose = true;
+    static boolean materialize = false;
+    static String kgOutput = "src/main/kg_output/";
+    static String greenhouseAssetModel = "src/main/resources/greenhouse.ttl";
+    static String domainPrefix = "http://www.semanticweb.org/gianl/ontologies/2023/1/sirius-greenhouse#";
+    static String progPrefix = "https://github.com/Edkamb/SemanticObjects/Program#";
+    static String runPrefix = "https://github.com/Edkamb/SemanticObjects/Run" + System.currentTimeMillis() + "#";
+    static String langPrefix = "https://github.com/Edkamb/SemanticObjects#";
+    static HashMap<String, String> extraPrefixes = new HashMap<>();
+    static boolean useQueryType = false;
 
-        Settings settings = new Settings(true, true,
-            "C:\\Users\\Gianl\\Desktop\\UniTo\\internship\\greenhouse_twin_project" +
-                "\\smol_runner\\src\\main\\kg_output",
-            "C:\\Users\\Gianl\\Desktop\\UniTo\\internship\\greenhouse_twin_project" +
-                "\\smol_runner\\src\\main\\resources\\greenhouse.ttl",
-            "ast:",
-            "https://github.com/Edkamb/SemanticObjects/Program#",
-            "https://github.com/Edkamb/SemanticObjects/Run" + timeMilli + "#",
-            "https://github.com/Edkamb/SemanticObjects#",
-            new HashMap<String, String>(),
-            false);
+    public static void main(String[] args) {
+        Settings settings = getSettings();
 
         REPL repl = new REPL(settings);
 
         repl.command("verbose", "true");
+
         repl.command("read",
-            "\"C:\\Users\\Gianl\\Desktop\\UniTo\\internship\\greenhouse_twin_project" +
-                "\\smol_runner\\src\\main\\resources\\greenhouse.smol\"");
+            "src/main/resources/greenhouse.smol");
+
         repl.command("auto", "");
         repl.command("dump", "out.ttl");
+    }
+
+    @NotNull
+    private static Settings getSettings() {
+        String assetModel = getAssetModel();
+
+        return new Settings(
+            SmolRunner.verbose,
+            SmolRunner.materialize,
+            SmolRunner.kgOutput,
+            assetModel,
+            SmolRunner.domainPrefix,
+            SmolRunner.progPrefix,
+            SmolRunner.runPrefix,
+            SmolRunner.langPrefix,
+            SmolRunner.extraPrefixes,
+            SmolRunner.useQueryType
+        );
+    }
+
+    private static String getAssetModel() {
+        // Read the asset model from the file in SmolRunner.greenhouseAssetModel
+        try {
+            return Files.readString(new File(SmolRunner.greenhouseAssetModel).toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
